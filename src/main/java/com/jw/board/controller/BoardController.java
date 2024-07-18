bdaccb1aee1beb704252b81b7d3c1da21661207d
김지우 <113417749+recordmystory@users.noreply.github.com>
2024-07-18 15:20:51 +0900

    remove : 불필요한 import문 제거

--- src/main/java/com/jw/board/controller/BoardController.java
+++ src/main/java/com/jw/board/controller/BoardController.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
