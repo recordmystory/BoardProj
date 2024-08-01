b555546d1b8f8f4964df748e48f30b8afacedbdf
김지우 <113417749+recordmystory@users.noreply.github.com>
2024-08-01 16:16:29 +0900

    remove : PagingUtil
    - 공백 제거
    - if문 블록 제거

--- src/main/java/com/jw/common/util/PagingUtil.java
+++ src/main/java/com/jw/common/util/PagingUtil.java
@@ -18,13 +18,11 @@ public class PagingUtil {
 	 */
 	public static PageInfoVO getPageInfo(int listCount, int currentPage, int pageLimit, int boardLimit) {
 
-		int maxPage = (int) Math.ceil((double) listCount / boardLimit) == 0 ? 1
-				: (int) Math.ceil((double) listCount / boardLimit);
+		int maxPage = (int) Math.ceil((double) listCount / boardLimit) == 0 ? 1 : (int) Math.ceil((double) listCount / boardLimit);
 		int startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
 		int endPage = startPage + pageLimit - 1;
-		if (endPage > maxPage) {
-			endPage = maxPage;
-		}
+		if (endPage > maxPage) endPage = maxPage;
+		
 		return new PageInfoVO(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage, endPage);
 	}
 }
